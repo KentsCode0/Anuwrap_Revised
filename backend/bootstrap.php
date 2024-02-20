2e07fc62ddb11a23d633ee08d21d71880e0d7e06
Adrian Gallano <adriangallanoengr@gmail.com>
2024-02-20 13:43:31 +0800

    Add database configuration

---
+++ backend/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+require "../vendor/autoload.php";
+use Dotenv\Dotenv;
+
+use Src\Config\Database;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$pdo = (new Database())->get_connection();
