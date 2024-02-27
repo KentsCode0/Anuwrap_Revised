10e6ed2978b699a7262a11506eb0c4abd854a55f
Adrian Gallano <adriangallanoengr@gmail.com>
2024-02-28 00:36:26 +0800

    Rename session to token and refactor controller

--- backend/src/controllers/TokenController.php
+++ backend/src/controllers/TokenController.php
@@ -3,10 +3,9 @@
 namespace Src\Controllers;
 
 use Src\Models\Authentication;
-use Src\Models\User;
 use Src\Config\DatabaseConnector;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
+use Src\Services\AuthenticationService;
+
 
 class SessionController
 {
@@ -18,43 +17,31 @@ class SessionController
         $this->authentication = new Authentication($this->pdo);
     }
 
-    function postSession()
+    function postToken()
     {
         if (isset($_POST['email']) && isset($_POST['password'])) {
-            #sanitation
             $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
             $password = $_POST['password'];
 
             $user = $this->authentication->get("email", $email);
 
-            if ($user && password_verify($password, $user['password'])) {
+            $AuthService = new AuthenticationService();
 
-                $payload = array(
-                    "userID" => $user["user_id"],
-                );
-                $token = JWT::encode(
-                    $payload,
-                    $_ENV['SECRET_API_KEY'],
-                    "HS256"
-                );
+            $isAuthenticated = $AuthService->authenticate($user, $password);
 
-                setcookie("Token", $token, [
-                    'expires' => time() + 3600,
-                    'path' => '/',
-                    'domain' => '',
-                    'secure' => true,
-                    'httponly' => true
-                ]);
-
+            if ($isAuthenticated) {
+                http_response_code(200);
                 unset($user['password']);
                 echo json_encode(array("success" => true, "user" => $user));
             } else {
                 http_response_code(401);
-                echo json_encode(array("success" => false, "message" => "Incorrect email or password"));
+                echo json_encode(array("success" => false, "message" => "Incorrect email or password "));
             }
         } else {
             http_response_code(400);
             echo json_encode(array("success" => false, "message" => "Email and password are required"));
         }
     }
+
+    
 }
