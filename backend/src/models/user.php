e30112ac688813421c2a137e9847d1781762b0f5
Adrian Gallano <adriangallanoengr@gmail.com>
2024-02-29 00:29:15 +0800

    Add update in user models

--- backend/src/models/user.php
+++ backend/src/models/user.php
@@ -14,7 +14,7 @@ class User
 
     function get($id)
     {
-        
+
         if (!$id) return false;
 
         $queryStr = "SELECT * FROM user WHERE user_id=:id";
@@ -24,13 +24,12 @@ class User
             $stmt->execute(array(
                 "id" => $id
             ));
+            $user = $stmt->fetch();
+            return $user;
         } catch (PDOException $e) {
             error_log($e->getMessage());
             return null;
         }
-
-        $user = $stmt->fetch();
-        return $user;
     }
 
     function create($user)
@@ -63,15 +62,42 @@ class User
         }
     }
 
-    function update($id)
+    function update($id, $request)
     {
         if (!$id) return false;
+
+        $username = $request['username'];
+        $firstname = $request['firstname'];
+        $lastname = $request['lastname'];
+
+        $queryStr = "UPDATE user 
+        SET username=:username,
+        first_name=:firstname,
+        last_name=:lastname
+        WHERE user_id = :id";
+
+        $stmt = $this->pdo->prepare($queryStr);
+        try {
+            $stmt->execute(
+                array(
+                    "username" => $username,
+                    "firstname" => $firstname,
+                    "lastname" => $lastname,
+                    "id" => $id
+                )
+            );
+            return true;
+        } catch (PDOException $e) {
+            echo $e;
+            error_log($e->getMessage());
+            return false;
+        }
     }
     function delete($id)
     {
         if (!$id) return false;
 
-        $queryStr = "DELETE FROM user WHERE $id = :id";
+        $queryStr = "DELETE FROM user WHERE user_id = :id";
 
         $stmt = $this->pdo->prepare($queryStr);
 
