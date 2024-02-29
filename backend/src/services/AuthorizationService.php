6334e765b8eb4c1048e7adbde714539b2855e01b
Adrian Gallano <adriangallanoengr@gmail.com>
2024-02-29 23:24:47 +0800

    Add auth token match

--- backend/src/services/AuthorizationService.php
+++ backend/src/services/AuthorizationService.php
@@ -5,4 +5,11 @@ namespace Src\Services;
 date_default_timezone_set("Asia/Manila");
 class AuthorizationService
 {
+    function isTokenMatch($id)
+    {
+        $tokenService = new TokenService();
+        $token = $tokenService->readEncodedToken();
+
+        return $token && $token['user_id'] == $id;
+    }
 }
