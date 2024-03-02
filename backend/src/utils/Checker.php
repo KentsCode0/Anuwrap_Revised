1c3ab39746d5eca220e251173490da48a7419815
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-03 00:57:59 +0800

    Add checker utility

---
+++ backend/src/utils/Checker.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src\Utils;
+class Checker
+{
+    static function isFieldExist($request, $args)
+    {
+        foreach ($args as $field) {
+            if (!(isset($request[$field]))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
