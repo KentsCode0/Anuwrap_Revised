ac81dea0378f6a334596de2c38a239b115c7b566
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-05 00:34:34 +0800

    Add auto generate tables

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -2,6 +2,20 @@
 require "../vendor/autoload.php";
 
 use Dotenv\Dotenv;
+use Src\Config\DatabaseConnector;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
+
+try {
+    $pdo = (new DatabaseConnector)->getConnection();
+    $sql = file_get_contents(__DIR__ . "\migrations\anuwrap_v5.sql");
+    $statements = explode(';', $sql);
+    foreach ($statements as $statement) {
+        if (!empty(trim($statement))) {
+            $pdo->exec($statement);
+        }
+    }
+} catch (PDOException $e) {
+    error_log($e->getMessage());
+}
