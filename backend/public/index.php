4072bfe717c1bed8d589752cfa5ea2a1d850d75f
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-04 11:39:11 +0800

    Fix cors error

--- backend/public/index.php
+++ backend/public/index.php
@@ -10,9 +10,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-// Set content type
 header("Content-Type: application/json; charset=UTF-8");
+header("HTTP/1.1 200 OK");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
