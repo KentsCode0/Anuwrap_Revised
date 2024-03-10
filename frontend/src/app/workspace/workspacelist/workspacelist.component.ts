5706c6f32fa864a3754ff7b82ffeb05edb5fee24
Kent <belenkentharold@gmail.com>
2024-03-10 20:44:20 +0800

    add getworkspace

--- frontend/src/app/workspace/workspacelist/workspacelist.component.ts
+++ frontend/src/app/workspace/workspacelist/workspacelist.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { AuthService } from '../../auth/auth.service';
+import { TokenService } from '../../auth/token/token.service';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
     selector: 'app-workspacelist',
@@ -21,7 +24,22 @@ export class WorkspacelistComponent implements OnInit {
     constructor(private authService: AuthService){}
 
     ngOnInit(): void {
+        const headers = TokenService.headers;
+        const id = TokenService.getUserId();
 
+        this.authService.getWorkspace(id, headers).subscribe(
+            (response) => {
+                this.workspace.workspaceId = response.data.workspace.workspace_id;
+                this.workspace.name = response.data.workspace.name;
+                this.workspace.dateModified = response.data.workspace.date_modified;
+                this.workspace.name = response.data.workspace.date_created;
+                console.log('User Workspace:', response.data.workspace.name)
+
+            },
+            (error) => {
+                console.log('error fetching user workspaces:', error);
+            }
+        );
     }
 
 }
