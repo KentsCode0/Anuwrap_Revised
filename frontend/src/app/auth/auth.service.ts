2ab6714e4762fa9d9744f3a4a5212912eedc616a
Adrian Gallano <adriangallanoengr@gmail.com>
2024-03-10 13:32:20 +0800

    Fix errors

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -6,13 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-<<<<<<< HEAD
   private apiUrl ='http://localhost/anuwrap/backend/public/api';
   
-=======
-  private apiUrl = 'http://localhost/anuwrap/backend/public/api';
-
->>>>>>> 0d953316352f4decd89dfb634aa9ad9efd7dd2fd
   constructor(private http: HttpClient) {}
 
   register(user: any): Observable<any> {
@@ -20,20 +15,12 @@ export class AuthService {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     
     // Send a POST request to your backend API to register the user
-<<<<<<< HEAD
     return this.http.post<any>(`${this.apiUrl}/user`, user, { headers });
-=======
-    return this.http.post<any>(`${this.apiUrl}/user`, user);
->>>>>>> 0d953316352f4decd89dfb634aa9ad9efd7dd2fd
   }
   
   login(credentials: { email: string, password: string }): Observable<any> {
     
     // Send a POST request to your backend API to authenticate the user
-<<<<<<< HEAD
     return this.http.post<any>(`${this.apiUrl}/token`, credentials, { observe: 'response', responseType: 'json' });
-=======
-    return this.http.post<any>(`${this.apiUrl}/token`, credentials);
->>>>>>> 0d953316352f4decd89dfb634aa9ad9efd7dd2fd
   }
 }
