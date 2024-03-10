1b0f8e9a80bcac345a3acc193c7939fdad11ce47
Eunille Jan Apo <beerusgaming24@gmail.com>
2024-03-10 15:10:17 +0800

    First Commit Workspace and workspacelist

--- frontend/src/app/workspace/navbar/navbar.component.ts
+++ frontend/src/app/workspace/navbar/navbar.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-editprofile',
+  selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule],
-  templateUrl: './editprofile.component.html',
-  styleUrl: './editprofile.component.css'
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
 })
-export class EditprofileComponent {
+export class NavbarComponent {
 
 }
