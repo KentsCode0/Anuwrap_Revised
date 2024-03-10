71c854d6db9b446cf319db8151023669a3056c63
kel <111582635+Ashlikiyer@users.noreply.github.com>
2024-03-10 21:53:39 +0800

    created Profile and edit profile

--- frontend/src/app/workspace/profile/editprofile/editprofile.component.ts
+++ frontend/src/app/workspace/profile/editprofile/editprofile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavigationComponent } from '../../navigation/navigation.component';
 
 @Component({
   selector: 'app-editprofile',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavigationComponent],
   templateUrl: './editprofile.component.html',
   styleUrl: './editprofile.component.css'
 })
