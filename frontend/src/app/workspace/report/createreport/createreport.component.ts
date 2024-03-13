8033f7c8c760864821d56f68b6091ea4202c9c38
Eunille Jan Apo <beerusgaming24@gmail.com>
2024-03-13 10:26:47 +0800

    Fixed the open icon in the workspacelist

--- frontend/src/app/workspace/report/createreport/createreport.component.ts
+++ frontend/src/app/workspace/report/createreport/createreport.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from "../../navigation/navigation.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from 'express';
 
 @Component({
     selector: 'app-createreport',
     standalone: true,
     templateUrl: './createreport.component.html',
     styleUrl: './createreport.component.css',
-    imports: [RouterModule, NavigationComponent]
+    imports: [RouterModule, NavigationComponent, CommonModule, FormsModule]
 })
 export class CreatereportComponent {
+    // Property to control the visibility of the modal
+    openModal: boolean = false;
 
+    // Method to toggle the modal's visibility
+    toggleModal() {
+        this.openModal = !this.openModal;
+    }
+
+    createReport() {
+        // Method to handle creating the report
+    }
 }
